09c636cbe09e32452aaa4f73fb20b83e1756db25
ThiagoBauerBix <thiago.bauer@bix-tech.com>
2024-01-15 17:12:52 -0300

    changed message

--- src/pages/Scan/index.tsx
+++ src/pages/Scan/index.tsx
@@ -175,8 +175,8 @@ export default function Scan() {
               <div className="bg-green-600 text-white font-bold rounded-t px-4 py-2 text-sm">
                   Warning
               </div>
-              <div className="border border-t-0 border-green-400 rounded-b bg-green-100 px-4 py-3 text-green-700 text-sm">
-                All tasks finished. None assigned.
+              <div className="border border-t-0 border-green-400 rounded-b bg-green-100 px-4 py-3 text-green-700 text-sm"> 
+                The technician currently has no pending tasks for this car job.
               </div>
           </div>
         }
